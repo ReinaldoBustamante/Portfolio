840f130d81d4f29e4f55798afdbf62e8f57c8ea7
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2024-11-29 16:15:29 -0300

    fix: show images on projects

--- src/portfolio/pages/Projects.tsx
+++ src/portfolio/pages/Projects.tsx
@@ -7,7 +7,7 @@ export const Projects = () => {
       <div className="project-list">
 
         <div className="project-item">
-          <img src="/src/assets/project-portfolio.png" alt="" />
+          <img src="https://portfolio-images-reinaldo.s3.sa-east-1.amazonaws.com/project-portfolio.png" alt="" />
           <h2 className="project-title">Portafolio personal</h2>
           <p>Portafolio personal, diseñado para destacar mis habilidades como desarrollador full stack, mi experiencia y los proyectos personales en los que he trabajado.</p>
           <Technologies techs={[{ id: 1, name: 'React.js' }, { id: 2, name: 'Typescript' }, { id: 3, name: 'Sass' }]} />
@@ -18,7 +18,7 @@ export const Projects = () => {
         </div>
 
         <div className="project-item">
-          <img src="/src/assets/project-portfolio.png" alt="" />
+          <img src="https://portfolio-images-reinaldo.s3.sa-east-1.amazonaws.com/project-portfolio.png" alt="" />
           <h2 className="project-title">Portafolio personal</h2>
           <p>Portafolio personal, diseñado para destacar mis habilidades como desarrollador full stack, mi experiencia y los proyectos personales en los que he trabajado.</p>
           <Technologies techs={[{ id: 1, name: 'React.js' }, { id: 2, name: 'Typescript' }, { id: 3, name: 'Sass' }]} />
