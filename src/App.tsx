a9d1242e86b7d25c3174b35af621d6871991e955
Reinaldo Bustamante <reinaldo.bustamante026@gmail.com>
2024-11-28 22:49:32 -0300

    feat: Added loading page

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,41 @@ import { Navbar } from "./ui/components/Navbar"
 import { Sidebar } from "./ui/components/Sidebar";
 import { useTheme } from "./ui/hooks/useTheme";
 import { useSide } from "./ui/hooks/useSide";
+import { useEffect, useState } from "react";
+import { Loading } from "./portfolio/pages/Loading";
 
 export const App = () => {
   const { darkMode, toggleMode } = useTheme();
   const { showSide, openSide, closeSide } = useSide()
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className={darkMode ? 'dark' : 'light'}>
-      <header>
-        <Navbar darkMode={darkMode} toggleMode={toggleMode} openSide={openSide} />
-      </header>
-      <nav>
-        <Sidebar showSide={showSide} closeSide={closeSide} />
-      </nav>
-      <main className="pages">
-        <PortfolioRoutes />
-      </main>
+      {
+        showContent ? (
+          <>
+            <header>
+              <Navbar darkMode={darkMode} toggleMode={toggleMode} openSide={openSide} />
+            </header>
+            <nav>
+              <Sidebar showSide={showSide} closeSide={closeSide} />
+            </nav>
+            <main className="pages">
+              <PortfolioRoutes />
+            </main>
+          </>
+        ) : (
+          <Loading />
+        )
+      }
     </div>
   )
 }
